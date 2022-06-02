8f6f593ecdab1bb77c222167c7feaa2d67d5b7a1
Andrey Fabricio <andrey.fas@gmail.com>
2022-06-01 21:22:15 -0300

    Categories

---
+++ src/views/Category/CategoryView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h3 class="pt-3">Our Categories</h3>
+      </div>
+    </div>
+    <div class="row"></div>
+  </div>
+</template>
+<script>
+// const axios = require("axios");
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped></style>
